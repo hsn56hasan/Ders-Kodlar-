45fe2d3a26298225570402c42143f8beebc0f28d
hsn56hasan <119723963+hsn56hasan@users.noreply.github.com>
2023-04-08 10:03:44 +0300

    Add files via upload

---
+++ boy kilo ortalaması.py
@@ -0,0 +1,6 @@
+boy = float(input("Boy:"))
+kilo = int(input("Kilo:"))
+
+print("Beden kilo indeksi:", kilo  / (boy ** 2))
+
+print("30 Ortalama altındaysa zayıf , üstündeyse normal derecedesin")
---
+++ sayı çarpma.py
@@ -0,0 +1,8 @@
+
+a  =  int ( input ( "a:" ))
+b  =  int ( input ( "b:" ))
+c  =  int ( input ( "c:" ))
+
+carpisma  =  a  *  b  *  c
+
+print ( " {} x {} x {} = {} dir" . format ( a , b , c , carpisma ))
